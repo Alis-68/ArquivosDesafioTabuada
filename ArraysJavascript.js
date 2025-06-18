67f3b93428f3a0f85fbff1ddd194ba3b2731637a
Alisson Reis <0001153671@senaimgaluno.com.br>
2025-06-18 11:38:27 -0300

    Arquivo Arrays no Javascript

---
+++ ArraysJavascript.js
@@ -0,0 +1,35 @@
+// let Discos = ["Billie Jean","Sweet Child O'Mine","Gostava Tanto de Você",["Michael Jackson","Guns and Roses","Tim Maia"]];
+// console.log(Discos.unshift[0] = "Good Bye Yellow Brick Road");
+// console.log(Discos.unshift[4] = "Elton John");
+
+
+
+// let musicas = ["Good byes", "Congratulations", "All a Need", "Renegade", "Under The Influence", "Let Me Love You", ["Travis Scott", "Drake", "Ariana Grande"]];
+// console.log(musicas.unshift[0] = "Consume");
+// console.log(musicas.unshift[5] = "Stop Crying Your Heart Out");
+
+
+// let Quadrinhos = ["Demolidor O Homem Sem Medo", "Maus", "Reino do Amanhã", ["Frank Miller", "Art Spielgman", "Kurt Busiek"]];
+// console.log(Quadrinhos.push[3] = "Saga")
+// console.log(Quadrinhos.push[4] = "Brian K. Vaughan")
+
+// let manga = ["Jujutsu Kaisen", "ONE PUNCH MAN", "Demon Slayer", ["Black clover", "Tokyo Ghoul", "Nanatsu No Tazai"]];
+// console.log(manga.push[3] = "Berserk")
+// console.log(manga.push[1] = "Blue lock")
+// console.log(manga)
+
+
+
+// let ProgramasRuins = [" Big Brother Brasil ", " A Fazenda ", " Programas com Jacquin ", " Encontro com Patricia Poeta "];
+// ProgramasRuins .shift(3);
+// console.log(ProgramasRuins);
+
+
+// let personagensPoderosos = ["Rimuru", "Anos", "Hajun", "Madoka"];
+// personagensPoderosos.shift(3);
+// console.log(personagensPoderosos);
+
+
+let Séries = ["The Wire", "A Grande Familia", "Todo Mundo Odeia o Chris", "Breaking Bad", "Familia Soprano"];
+Séries.splice(1);
+console.log(Séries);
